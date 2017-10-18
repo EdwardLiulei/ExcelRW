36051f99b77705dd5e37f271bc845a9df575372b
EdwardLiulei <qingshi5230@163.com>
2017-10-18 13:46:22 +0800

    Add Projects

---
+++ ExcelRW/Base/ExcelWorkSheetBase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+
+
+namespace ExcelReadAndWrite.Base
+{
+    public abstract class ExcelWorkSheetBase
+    {
+        #region Field
+
+        private string _sheetName;
+
+        private DataTable _tableContent;
+
+        private bool _hasHeader;
+
+        #endregion
+
+        #region Properity
+
+        public string SheetName 
+        {
+            get { return _sheetName; }
+        }
+        
+        #endregion
+
+        #region Abstract Functions
+
+        public abstract string GetCellValue(int rowNumber,int columNumber);
+
+        #endregion
+
+    }
+
+}
---
+++ ExcelRW/Base/ExcelWorkbookBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExcelReadAndWrite.Base
+{
+    public abstract class ExcelWorkbookBase
+    {
+        #region Filed
+        protected List<string> _workSheets;
+
+        #endregion
+
+        #region Properity
+
+        public List<string> WorkSheets { get { return _workSheets; } }
+        #endregion
+
+        #region Absrtract Fuctions
+
+        public abstract void LoadWorkBook(string fileName);
+
+        public abstract void SaveWorkBook(string fileName);
+
+        public abstract ExcelWorkSheetBase GetSheet(string sheetName);
+
+        public abstract void SaveWorkSheet();
+        #endregion
+    }
+}
