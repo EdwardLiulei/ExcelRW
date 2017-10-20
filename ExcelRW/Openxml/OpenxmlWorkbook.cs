dc58184278ac3f246f6621bbb38461de5e0479b9
EdwardLiulei <qingshi5230@163.com>
2017-10-20 23:06:45 +0800

    update

---
+++ ExcelRW/Openxml/OpenxmlWorkbook.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelReadAndWrite.StdExcelModel;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace ExcelReadAndWrite.Openxml
+{
+    public class OpenxmlWorkbook : ExcelWorkbookBase
+    {
+        private WorkbookPart _xmlWorkbook;
+        public override ExcelWorkSheetBase GetSheet(string sheetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Load(string fileName)
+        {
+            var xmlDoc = SpreadsheetDocument.Open(fileName, true);
+            _xmlWorkbook =  xmlDoc.WorkbookPart;
+            var sheets = _xmlWorkbook.Workbook.Descendants<Sheet>();
+            foreach (Sheet sheet in _xmlWorkbook.Workbook.Descendants<Sheet>())
+            {
+                WorksheetPart worksheet = (WorksheetPart)_xmlWorkbook.GetPartById(sheet.Id);
+
+            }
+            
+        }
+
+        public override void Save(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExcelRW/Openxml/OpenxmlWorksheet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using ExcelReadAndWrite.StdExcelModel;
+
+namespace ExcelReadAndWrite.Openxml
+{
+    public class OpenxmlWorksheet : ExcelWorkSheetBase
+    {
+
+
+        
+        public override string GetCellValue(int rowNumber, int columNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DataTable GetTableContent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
